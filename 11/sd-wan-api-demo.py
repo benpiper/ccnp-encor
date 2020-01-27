b2b2231f545bdbe36412b424942cd3de338a6db4
Ben Piper <ben@benpiper.com>
2020-01-26 22:28:14 -0500

    Fix comment

--- 11/sd-wan-api-demo.py
+++ 11/sd-wan-api-demo.py
@@ -10,7 +10,7 @@ data =  {
         }
 url = "https://sandboxsdwan.cisco.com:8443/j_security_check"
 response = s.post(url, data=data, verify=False)
-# Use the vManage API to get device details for all vEdge Cloud (cEdge) routers.
+# Use the vManage API to view the OMP routes being advertised to the vEdge Cloud (cEdge) router with the IP address 4.4.4.60.
 url = "https://sandboxsdwan.cisco.com:8443/dataservice/device/omp/routes/received?deviceId=4.4.4.60"
 response = s.get(url, data=data, verify=False)
 # The response contains column heading definitions, so to show only the interesting information, we'll restrict the output to the "data" object.
