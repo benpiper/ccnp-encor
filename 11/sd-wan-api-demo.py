36087c634ac4929b1371831fec7433549ff6c1f7
Ben Piper <ben@benpiper.com>
2020-01-26 22:26:06 -0500

    Add vManage API demo

---
+++ 11/sd-wan-api-demo.py
@@ -0,0 +1,17 @@
+import json
+import requests
+import urllib3
+from requests.auth import HTTPBasicAuth
+# Authenticate to the vManage controller. It uses cookies for authentication, so we won't have to manually store and pass a token as before.
+s = requests.Session()
+data =  {
+            'j_username':  "devnetuser",
+            'j_password':  "Cisco123!"
+        }
+url = "https://sandboxsdwan.cisco.com:8443/j_security_check"
+response = s.post(url, data=data, verify=False)
+# Use the vManage API to get device details for all vEdge Cloud (cEdge) routers.
+url = "https://sandboxsdwan.cisco.com:8443/dataservice/device/omp/routes/received?deviceId=4.4.4.60"
+response = s.get(url, data=data, verify=False)
+# The response contains column heading definitions, so to show only the interesting information, we'll restrict the output to the "data" object.
+print(json.dumps(response.json()["data"], indent=2))
